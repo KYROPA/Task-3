8cbd7feb2cfab547816e66f37865a515ea90e0e8
Андрей Курец <kyropa15@hotmail.com>
2019-09-12 20:08:24 +0300

    Changes 1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace Task_2_week_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string answer = "тень";
+            string option = "";
+            do
+            {
+                Console.WriteLine("Ты предстал перед могучим сфинксом. Внимай моим загадкам - говорит он.");
+                Console.WriteLine($"Его не потрогаешь и не увидишь " +
+                    $"\nФормы нет у него." +
+                    $"\nНет вообще ничего – только имя." +
+                    $"\nОно коротким бывает, то длинным," +
+                    $"\nИ оно всегда с нами," +
+                    $"\nВо всех разговорах, забавах и играх.");
+
+                option = Console.ReadLine();
+
+            }
+            while (option != answer);
+
+
+                
+
+
+
+
+            
+            
         }
     }
 }
