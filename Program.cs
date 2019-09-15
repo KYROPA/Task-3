700dfea6b80d597e2bfa634efb58039dbf85ae63
Андрей Курец <kyropa15@hotmail.com>
2019-09-15 21:50:21 +0300

    3 riddlers, 3 answers, one by one.

--- Program.cs
+++ Program.cs
@@ -10,19 +10,21 @@ namespace Task_2_week_2
         static void Main(string[] args)
         {
             string option = "";
-            string answer = "shadow";
+            string answer1 = "shadow";
+            string answer2 = "bonfire";
+            string answer3 = "darkness";
             int i = 0;
             string[] meeting = File.ReadAllLines("Friendly fire.txt");
             Console.WriteLine(meeting[0]);
             Console.WriteLine();
             do
             {
-                string riddler1 = File.ReadAllText("Riddlers.txt");
+                string riddler1 = File.ReadAllText("Riddler1.txt");
                 Console.WriteLine(riddler1);
                 Console.WriteLine($"\nWrite your answer, buddy.\n");
                 i++;
                 option = Console.ReadLine();
-                if (answer == option)
+                if (answer1 == option)
                 {
                     Console.WriteLine($"\nYEP, sir, it's correct\n");
                 }
@@ -31,8 +33,43 @@ namespace Task_2_week_2
                     Console.WriteLine($"\nNO, it's wrong, try again, buddy\n");
                 }
             }
-            while (option.ToLower() != answer.ToLower() && i < 3);
-            Console.WriteLine("GAME OVER");
+            while (option.ToLower() != answer1.ToLower() && i < 3);
+            
+            do
+            {
+                string riddler2 = File.ReadAllText("Riddler2.txt");
+                Console.WriteLine(riddler2);
+                Console.WriteLine($"\nWrite your answer, buddy.\n");
+                i++;
+                option = Console.ReadLine();
+                if (answer2 == option)
+                {
+                    Console.WriteLine($"\nYEP, sir, it's correct\n");
+                }
+                else
+                {
+                    Console.WriteLine($"\nNO, it's wrong, try again, buddy\n");
+                }
+            }
+            while (option.ToLower() != answer2.ToLower() && i < 3);
+
+            do
+            {
+                string riddler3 = File.ReadAllText("Riddler3.txt");
+                Console.WriteLine(riddler3);
+                Console.WriteLine($"\nWrite your answer, buddy.\n");
+                i++;
+                option = Console.ReadLine();
+                if (answer3 == option)
+                {
+                    Console.WriteLine($"\nYEP, sir, it's correct\n");
+                }
+                else
+                {
+                    Console.WriteLine($"\nNO, it's wrong, try again, buddy\n");
+                }
+            }
+            while (option.ToLower() != answer3.ToLower() && i < 3);
         }
     }
 }
