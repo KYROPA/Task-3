34d6a28713d2a9a04925e93e9dd1d4c076c8f913
Андрей Курец <kyropa15@hotmail.com>
2019-09-15 20:49:07 +0300

    New changes

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Text;
 using System.Collections.Concurrent;
-using System.Text;
 
 namespace Task_2_week_2
 {
@@ -13,17 +12,29 @@ namespace Task_2_week_2
             string option = "";
             string answer = "shadow";
             int i = 0;
-          
+            string[] meeting = File.ReadAllLines("Friendly fire.txt");
+            Console.WriteLine(meeting[0]);
+            Console.WriteLine();
             do
             {
                 string riddler1 = File.ReadAllText("Riddlers.txt");
                 Console.WriteLine(riddler1);
-                
+                Console.WriteLine($"\nWrite your answer, buddy.\n");
+
+
                 i++;
                 option = Console.ReadLine();
-
+                if (answer == option)
+                {
+                    Console.WriteLine($"\nYEP, sir, it's correct\n");
+                }
+                else
+                {
+                    Console.WriteLine($"\nNO, it's wrong, try again, buddy\n");
+                }
             }
             while (option.ToLower() != answer.ToLower() && i < 3);
+            Console.WriteLine("GAME OVER");
         }
     }
 }
